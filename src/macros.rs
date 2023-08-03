1c2b208842d8466dd78039db064ae4ce09ba3029
Julian Rosse <julian@helixbass.net>
2023-08-03 08:36:27 -0400

    macros

--- src/macros.rs
+++ src/macros.rs
@@ -16,3 +16,11 @@ macro_rules! return_default_if_false {
         }
     };
 }
+
+#[macro_export]
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: std::sync::OnceLock<regex::Regex> = std::sync::OnceLock::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
+}

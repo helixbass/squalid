ca9515c6fc9291ab96ada609f66ca6ad8338a2cf
Julian Rosse <julian@helixbass.net>
2023-11-21 11:00:22 -0500

    cow

--- src/cow.rs
+++ src/cow.rs
@@ -2,6 +2,7 @@ use std::{borrow::Cow, slice::SliceIndex};
 
 pub trait CowStrExt<'a> {
     fn sliced(&self, range: impl SliceIndex<str, Output = str>) -> Cow<'a, str>;
+    fn trimmed(&self) -> Cow<'a, str>;
 }
 
 impl<'a> CowStrExt<'a> for Cow<'a, str> {
@@ -9,6 +10,13 @@ impl<'a> CowStrExt<'a> for Cow<'a, str> {
         match self {
             Self::Borrowed(value) => value[range].into(),
             Self::Owned(value) => (&**value)[range].to_owned().into(),
+        }
+    }
+
+    fn trimmed(&self) -> Cow<'a, str> {
+        match self {
+            Self::Borrowed(value) => value.trim().into(),
+            Self::Owned(value) => value.trim().to_owned().into(),
         }
     }
 }

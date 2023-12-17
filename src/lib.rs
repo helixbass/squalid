5cca03f5ac479feaac4c6472d2335a0dfd8a63e0
Julian Rosse <julian@helixbass.net>
2023-12-17 07:39:26 -0500

    cow

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod vec;
 
 pub use crate::bool::BoolExt;
 pub use collections::Contains;
-pub use cow::CowStrExt;
+pub use cow::{CowExt, CowStrExt};
 pub use everything::EverythingExt;
 pub use hash_map::HashMapExt;
 pub use option::{IsEmpty, NonEmpty, OptionExt, OptionExtDeref, OptionExtIterator};

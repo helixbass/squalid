3f1d805ac826b96e61c8a4ee60d781132706cb5a
Julian Rosse <julian@helixbass.net>
2023-12-17 07:26:35 -0500

    cow

--- src/cow.rs
+++ src/cow.rs
@@ -92,22 +92,26 @@ where
 }
 
 pub trait CowStrExt<'a> {
-    fn sliced(&self, range: impl SliceIndex<str, Output = str>) -> Cow<'a, str>;
-    fn trimmed(&self) -> Cow<'a, str>;
+    fn sliced(self, range: impl SliceIndex<str, Output = str>) -> Cow<'a, str>;
+    fn sliced_ref(&self, range: impl SliceIndex<str, Output = str>) -> Cow<'a, str>;
+    fn trimmed(self) -> Cow<'a, str>;
+    fn trimmed_ref(&self) -> Cow<'a, str>;
 }
 
 impl<'a> CowStrExt<'a> for Cow<'a, str> {
-    fn sliced(&self, range: impl SliceIndex<str, Output = str>) -> Cow<'a, str> {
-        match self {
-            Self::Borrowed(value) => value[range].into(),
-            Self::Owned(value) => (&**value)[range].to_owned().into(),
-        }
+    fn sliced(self, range: impl SliceIndex<str, Output = str>) -> Cow<'a, str> {
+        self.map_borrowed(|value| &value[range])
     }
 
-    fn trimmed(&self) -> Cow<'a, str> {
-        match self {
-            Self::Borrowed(value) => value.trim().into(),
-            Self::Owned(value) => value.trim().to_owned().into(),
-        }
+    fn sliced_ref(&self, range: impl SliceIndex<str, Output = str>) -> Cow<'a, str> {
+        self.map_borrowed_ref(|value| &value[range])
+    }
+
+    fn trimmed(self) -> Cow<'a, str> {
+        self.map_borrowed(str::trim)
+    }
+
+    fn trimmed_ref(&self) -> Cow<'a, str> {
+        self.map_borrowed_ref(str::trim)
     }
 }
